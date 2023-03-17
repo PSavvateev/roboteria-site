e4436b548ace04a7f830669684253fa2536ebec3
PSavvateev <p.savvateev@gmail.com>
2023-03-17 10:30:53 +0100

    added gallery

--- app.js
+++ app.js
@@ -68,3 +68,92 @@ scrollLinks.forEach(function(link) {
 // **************** set date **************
 const date = document.querySelector('#date');
 date.innerHTML = new Date().getFullYear();
+
+
+
+
+// *************** gallery of projects ********************
+
+const projects = [
+    {
+        id: 1,
+        name: "names classifier",
+        info: "AI-based web application classifying gender by a given name.",
+        img: "./img/robo-tile-x25.svg",
+        link: "https://gender-app.roboteria.io",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    },
+    {
+        id: 2,
+        name: "project #2",
+        info: "another AI-based application. Here should be some short description.",
+        img: "./img/robo-tile-x25.svg",
+        link: "#",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    },
+    {
+        id: 3,
+        name: "project #3",
+        info: "another AI-based application. Here should be some short description.",
+        img: "./img/robo-tile-x25.svg",
+        link: "#",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    },
+    {
+        id: 4,
+        name: "project #4",
+        info: "another AI-based application. Here should be some short description.",
+        img: "./img/robo-tile-x25.svg",
+        link: "#",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    },
+    {
+        id: 5,
+        name: "project #5",
+        info: "another AI-based application. Here should be some short description.",
+        img: "./img/robo-tile-x25.svg",
+        link: "#",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    },
+    {
+        id: 6,
+        name: "project #6",
+        info: "another AI-based application. Here should be some short description.",
+        img: "./img/robo-tile-x25.svg",
+        link: "#",
+        stack: ["python", "flask", "html", "css"],
+        git: ""
+    }
+];
+
+const gallery = document.querySelector(".gallery")
+
+// load gallery
+
+window.addEventListener("DOMContentLoaded", function() {
+    displayGallery(projects);
+});
+
+// displaying projects function
+
+function displayGallery(projects) {
+    let displayProjects = projects.map( project => {
+        return `<a href="${project.link}">
+                    <article class="project">                   
+                        <header>
+                            <h3 class="name">${project.name}</h3>
+                            <p class="info">${project.info}</p>
+                        </header>
+                        <img src=${project.img} alt=${project.name}>                    
+                    </article>
+                </a>`
+    });
+
+    displayProjects = displayProjects.join("");
+    gallery.innerHTML = displayProjects;
+};
